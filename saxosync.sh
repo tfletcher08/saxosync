ace0e54d354b4d389ae6e8c2220874aaa339c810
Kevin Tindall <ktindall@gmti.gannett.com>
2015-06-16 16:45:10 -0400

    Updated script to check for deleted and modified files

--- saxosync.sh
+++ saxosync.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#LOCALPATH="/static/web/satatic/content/sites/NJ/asburypark/app/"
-LOCALPATH="/static/web/static/content/sites/NJ/asburypark/app/e-edition/"
-REMOTEPATH="/376025/web/static/content/NJ/asburypark/app/e-edition"
-#REMOTEPATH="/376025/web/static/content/NJ/asburypark/app/"
+LOCALPATH="/static/web/static/content/sites/NJ/asburypark/app/"
+REMOTEPATH="/376025/web/static/content/NJ/asburypark/app/"
+SITEURL="http://content-static.app.com/"
+
+#LOCALPATH="/static/web/static/content/sites/NJ/asburypark/app/e-edition/"
+#REMOTEPATH="/376025/web/static/content/NJ/asburypark/app/e-edition"
+
 REMOTEUSER="sshacs"
 REMOTEHOST="uscpcontent.upload.akamai.com"
 IDENTITYFILE="/root/.ssh/id_rsa"
 SSHOPTS="-i $IDENTITYFILE"
 RSYNCOPS=(-rnzl --size-only --out-format='%i:%n' --delete -e "ssh ${SSHOPTS[@]}")
+
+DELETEDFILES=()
+MODIFIEDFILES=()
 
 echo "Running sync.."
 echo "Local path: $LOCALPATH"
@@ -22,3 +28,17 @@ else
     echo "No log file?"
     exit 1
 fi
+
+DELETEDFILES=($(grep "*deleting" rsynclog.txt|cut -d ':' -f 2))
+MODIFIEDFILES=($(grep -E "<[fdLDS][.c][.s][.tT][.p][.o][.g][.u][.a][.x]" rsynclog.txt|cut -d ':' -f 2))
+
+echo "I will purge ${#DELETEDFILES[@]} deleted files and ${#MODIFIEDFILES[@]} modified files" 
+
+for i in ${DELETEDFILES[@]}; do
+    echo "Insert curl for purging $SITEURL$i here"
+done
+
+
+for i in ${MODIFIEDFILES[@]}; do
+    echo "Insert curl for purging $SITEURL$i here"
+done
