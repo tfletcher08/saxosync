aa4c806ceb603d904bbc3def2b90e2f490791a38
Kevin Tindall <ktindall@gmti.gannett.com>
2015-06-22 15:56:50 -0400

    Added Akamai cache purging

--- saxosync.sh
+++ saxosync.sh
@@ -4,6 +4,12 @@ LOCALPATH=$1
 REMOTEPATH=$2
 SITEURL=$3
 
+if [ -z $LOCALPATH ] || [ -z $REMOTEPATH ] || [ -z $SITEURL ]; then
+    echo "Usage: saxosync <Local path> <Remote path> <Base Site url>"
+    echo "Example: saxosync /static/web/static/content/sites/NJ/asburypark/app/ /376025/web/static/content/NJ/asburypark/app/ http://content-static.app.com/"
+    echo "Note: Local path and base site url must end in '/'"
+    exit 1
+fi
 
 REMOTEUSER="sshacs"
 REMOTEHOST="uscpcontent.upload.akamai.com"
@@ -19,12 +25,14 @@ echo "Running sync.."
 echo "Local path: $LOCALPATH"
 echo "Remote path: $REMOTEPATH"
 
+rm rsynclog.txt
+
 rsync "${RSYNCOPS[@]}" $LOCALPATH $REMOTEUSER@$REMOTEHOST:$REMOTEPATH |tee rsynclog.txt
 
 if [ -s rsynclog.txt ]; then
     echo "Log exists!"
 else
-    echo "No log file. Either no changes exist or there was an error, check job output.."
+    echo "No log file. Either no changes exist or there was an error, check job output."
     exit 1
 fi
 
@@ -33,8 +41,18 @@ MODIFIEDFILES=($(grep -E "<[fdLDS][.c][.s][.tT][.p][.o][.g][.u][.a][.x]|cL[.c][.
 
 echo "I will purge ${#DELETEDFILES[@]} deleted files and ${#MODIFIEDFILES[@]} modified files" 
 
+URLLIST=""
+
 for i in ${DELETEDFILES[@]} ${MODIFIEDFILES[@]}; do
-    echo "Insert curl for purging $SITEURL$i here"
+    if [ -z $URLLIST ]; then
+        URLLIST="\"$SITEURL$i\""
+    else
+        URLLIST="$URLLIST,\"$SITEURL$i\""
+    fi
 done
 
+URLLIST="{\"objects\":[$URLLIST]}"
 
+echo "Payload being sent to Akamai: $URLLIST"
+
+python ../edgegrid-curl/egcurl --eg-config $CCUCONFIGFILE -sSik -H "Content-Type: application/json" -X POST -d "$URLLIST" https://akab-uttkurwznqtmpbw5-3gz2fnxrham6cu5k.purge.akamaiapis.net/ccu/v2/queues/default
